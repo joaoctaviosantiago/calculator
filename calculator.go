7a1db6e5ab55e6eaf61fbf41fd7c789bfd0bfb39
João Octávio Santiago <jomsoutosantiago@gmail.com>
2024-08-26 19:55:57 -0300

    added the "division" function and its tests

--- calculator.go
+++ calculator.go
@@ -1,6 +1,8 @@
 // Package calculator does simple calculations.
 package calculator
 
+import "errors"
+
 // Add takes two numbers and returns the result of adding
 // them together.
 func Add(a, b float64) float64 {
@@ -17,4 +19,15 @@ func Subtract(a, b float64) float64 {
 // returns the result of multiplying them together
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+// Divide takes two numbers a and b, and returns
+// either the result of the division of a by b or
+// it reutrns an error in case the division is invalid
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero not allowed")
+	}
+
+	return a / b, nil
 }
